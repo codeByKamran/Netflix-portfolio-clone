29f39ae5cffe67cf87682a2913ddf363170d45c1
Kamran Ashraf <79129427+codeByKamran@users.noreply.github.com>
2021-02-17 11:36:21 +0500

    Update CopyrightSection.js

--- src/Components/CopyrightSection.js
+++ src/Components/CopyrightSection.js
@@ -2,26 +2,17 @@ import React from "react";
 
 const CopyrightSection = () => {
   return (
+    return (
     <div className="copyright__section">
       <h1>
-        @This
-        <a href="https://www.netflix.com/" target="_blank">
-          Netflix
+        ©This <strong>Netflix Clone Demo</strong> is Built by
+        <a href="https://kamran-personal-portfolio.netlify.app/" target="_blank">
+          Kamran A.
         </a>
-        <strong>Clone</strong> is built by
-        <a href="https://kamran-personal-portfolio.netlify.app" target="_blank">
-          Azhar Zaman
-        </a>
-        with
-        <a
-          href="https://medium.com/@thinkwik/why-reactjs-is-gaining-so-much-popularity-these-days-c3aa686ec0b3"
-          target="_blank"
-        >
-          React
-        </a>
-        to Showcase Skills@
+        with <strong>React</strong> to Showcase Skills©
       </h1>
     </div>
+  );
   );
 };
 
